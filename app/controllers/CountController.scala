80d7cd38b8f8d12d2a9e0a765f2d9f29a9a861df
MrDema <matteodema17@gmail.com>
2017-05-11 23:34:46 +0200

    partials e test verdi

--- app/controllers/CountController.scala
+++ app/controllers/CountController.scala
@@ -14,12 +14,11 @@ import services.Counter
  */
 @Singleton
 class CountController @Inject() (counter: Counter) extends Controller {
-
+	
   /**
    * Create an action that responds with the [[Counter]]'s current
    * count. The result is plain text. This `Action` is mapped to
    * `GET /count` requests by an entry in the `routes` config file.
    */
   def count = Action { Ok(views.html.index(counter.nextCount().toString)) }
-
 }
